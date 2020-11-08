aee42f955796328b5476ed53e410f7b2e5dd8f63
aa-ag <aaron.aguerrevere@gmail.com>
2020-11-08 16:09:39 -0600

    added gameover function to avoid drawing lines after one player wins

--- pygamev.py
+++ pygamev.py
@@ -24,7 +24,6 @@ screen.fill(BACKGROUND)
 
 # Board
 board = np.zeros((BOARD_ROWS, BOARD_COLS))
-# print(board)
 
 # Functions
 def draw_lines():
@@ -75,44 +74,118 @@ def check_win(player):
     drawing a vertical line, a horizonal line or 
     a diagonal line, asc or descending
     '''
+    # checks for vertical "col" wins
     for col in range(BOARD_COLS):
         if board[0][col] == player and board[1][col] == player and board[2][col] == player:
             draw_vertical_win(col, player)
             return True
 
+    # checks for horizontal "row" wins
     for row in range(BOARD_ROWS):
         if board[row][0] == player and board[row][1] == player  and board[row][2] == player:
             draw_horizontal_win(row, player)
             return True
 
+    # checks for diagonal "ascending" wins
     if board[2][0] == player and board[1][1] == player and board[0][2] == player:
         draw_asc_diagonal_win(player)
         return True
 
+    # checks for diagonal "descending" wins
     if board[0][0] == player and board[1][1] == player and board[2][2] == player:
         draw_desc_diagonal_win(player)
         return True 
-        
+
     return False
 
 def draw_vertical_win(col, player):
-    pass
+    '''
+    Draws vertical line to show vertical win
+    '''
+    pos_x = col * 200 + 100
+
+    if player == 1:
+        color = CIRCLE_COLOR
+    else:
+        color = X_COLOR
+
+    pygame.draw.line(screen, color, (pos_x, 15), (pos_x, HEIGHT - 15), 15)
 
 def draw_horizontal_win(row, player):
-    pass
+    '''
+    Draws horizonal line to show horizontal win
+    '''
+    pos_y = row * 200 + 100
+
+    if player == 1:
+        color = CIRCLE_COLOR
+    else:
+        color = X_COLOR
+
+    pygame.draw.line(screen, color, (15, pos_y), (WIDTH - 15, pos_y), 15)
 
 def draw_asc_diagonal_win(player):
-    pass
+    '''
+    Draws diagonal ascending, left-to-tight line to show win
+    '''
+    if player == 1:
+        color = CIRCLE_COLOR
+    else:
+        color = X_COLOR
+
+    pygame.draw.line(screen, color, (15, HEIGHT - 15), (WIDTH - 15, 15), 15)
 
 def draw_desc_diagonal_win(player):
-    pass
+    '''
+    Draws diagonal descending, right-to-left line to show win
+    '''
+    if player == 1:
+        color = CIRCLE_COLOR
+    else:
+        color = X_COLOR
+    
+    pygame.draw.line(screen, color, (15, 15), (WIDTH - 15, HEIGHT - 15), 15)
 
 def restart():
     pass
 
 player = 1
+game_over = False
+
+draw_lines()
+
+# main loop
+while True:
+    '''
+    Runs program (game) until players quit, or there are no more moves left, or one player wins
+    '''
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit
+        if event.type == pygame.MOUSEBUTTONDOWN and not game_over:
+            mouse_y = event.pos[1]
+            mouse_x = event.pos[0]
+            
+            clicked_row = int(mouse_y // 200)
+            clicked_col = int(mouse_x // 200)
+            
+            if square_available(clicked_row, clicked_col):
+                if player == 1:
+                    square_move(clicked_row, clicked_col, 1)
+                    if check_win(player):
+                        game_over = True
+                    player = 2
+                elif player == 2:
+                    square_move(clicked_row, clicked_col, 2)
+                    if check_win(player):
+                        game_over = True
+                    player = 1
+                draw_figures()
+
+    pygame.display.update()
 
 # TESTING
+# print(board)
 # square_move(0, 0, 1)
 # square_move(0, 1, 1)
 # square_move(0, 2, 1)
@@ -127,30 +200,4 @@ player = 1
 # square_move(0,0,1)
 # print(square_available(0,0))
 # print(full_board())
-
-draw_lines()
-
-# main loop
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            sys.exit
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            mouse_y = event.pos[1]
-            mouse_x = event.pos[0]
-            
-            clicked_row = int(mouse_y // 200)
-            clicked_col = int(mouse_x // 200)
-            
-            if square_available(clicked_row, clicked_col):
-                if player == 1:
-                    square_move(clicked_row, clicked_col, 1)
-                    player = 2
-                elif player == 2:
-                    square_move(clicked_row, clicked_col, 2)
-                    player = 1
-                draw_figures()
-
-    pygame.display.update()
-
-# TO DO: playing chips, playing functions || conda env: ttt
+# NOTES: conda env: ttt
